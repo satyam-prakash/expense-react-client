80940a9d4c0d36f2b8099f979feaa49dce4106ac
Satyam Prakash <prakashsatyam29@gmail.com>
2026-01-28 11:13:37 +0530

    examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Student from './pages/examples/Student'
+import Student1 from './pages/examples/Student1'
+import Student2 from './pages/examples/Student2'
+import Student3 from './pages/examples/Student3'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>Welcome to Expense App</h1> 
+        <Student />
+        <Student1 />
+        <Student2 />
+        <Student3 name='Tommy' rollNumber={20} percentage={45.0} />
+
     </>
   )
 }
